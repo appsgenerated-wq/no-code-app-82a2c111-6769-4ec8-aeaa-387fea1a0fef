2f3b96ea1796068e2b573dfafe0b8eed2cdd40b6
appsgenerated-wq <appsgenerated@gmail.com>
2025-10-07 16:54:02 -0700

    Update backend URL in constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,5 @@
 const config = {
-  BACKEND_URL: 'https://no-code-app-82a2c111-6769-4ec8-aeaa-387fea1a0fef-PLACEHOLDER.run.app',
+  BACKEND_URL: 'https://no-code-app-82a2c111-6769-4ec8-aeaa-387fea1a0fef-s7mzymgw4a-uc.a.run.app',
   APP_ID: '82a2c111-6769-4ec8-aeaa-387fea1a0fef'
 };
 
